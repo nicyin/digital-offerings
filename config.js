32548c3851ee6e1d2d2e752359151c01b9651a50
Nicci <hello@nicciyin.com>
2025-07-20 18:05:30 -0700

    updated config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: window.FIREBASE_API_KEY || "AIzaSyA4eIpYUBjAPVUcxK-JcEYwmu6nKcrn7Lk",
-  authDomain: window.FIREBASE_AUTH_DOMAIN || "digital-offerings.firebaseapp.com",
-  projectId: window.FIREBASE_PROJECT_ID || "digital-offerings",
-  storageBucket: window.FIREBASE_STORAGE_BUCKET || "digital-offerings.firebasestorage.app",
-  messagingSenderId: window.FIREBASE_MESSAGING_SENDER_ID || "992501647817",
-  appId: window.FIREBASE_APP_ID || "1:992501647817:web:16fe0ccb14c0c3e3a04018"
+  apiKey: window.FIREBASE_API_KEY,
+  authDomain: window.FIREBASE_AUTH_DOMAIN,
+  projectId: window.FIREBASE_PROJECT_ID,
+  storageBucket: window.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window.FIREBASE_MESSAGING_SENDER_ID,
+  appId: window.FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
